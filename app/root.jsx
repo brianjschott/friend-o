883a75df61a9884d7eac6d2ce32e454c711c7ab8
Brian Schott <bschott@cgps.org>
2023-11-15 14:32:16 -0500

    Changed name of app, starting rearrangement of components, imported CSS properly

--- app/root.jsx
+++ app/root.jsx
@@ -9,10 +9,18 @@ import {
 
 import {Button} from 'react-bootstrap';
 import FriendList from "./routes/allfriends";
-import bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import bootstrapCSS from 'bootstrap/dist/css/bootstrap.min.css';
 
 export const links = () => [
-    {rel: "stylesheet", href: bootstrap}
+    {rel: "stylesheet", href: bootstrapCSS}
+]
+
+export const meta = () => [
+
+]
+
+export const scripts = () => [
+
 ]
 
   export default function App() {
@@ -23,12 +31,11 @@ export const links = () => [
           <Links />
         </head>
         <body>
-          <h1>Friend-O App</h1>
+          <h1 class="display-1">Friend Zone</h1>
           <Outlet />
           <Scripts />
           <LiveReload />
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap**@5.1.0**/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
-
+          
         </body>
       </html>
     );
